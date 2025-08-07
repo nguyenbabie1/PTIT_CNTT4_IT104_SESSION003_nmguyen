a92617901b260baef803caa43df20ec8218620ae
nguyenbabie1 <brokeniuem569@gmail.com>
2025-08-07 07:55:49 +0700

    Update b7.ts

--- PTIT_CNTT4_IT104_SESSION003/PTIT_CNTT4_IT104_SESSION03_EXERCISE07/b7.ts
+++ PTIT_CNTT4_IT104_SESSION003/PTIT_CNTT4_IT104_SESSION03_EXERCISE07/b7.ts
@@ -1,16 +1,15 @@
+"use strict";
 // Khai báo biến kiểu string
 let str = "hello world";
-
 // Hàm loại bỏ ký tự trùng lặp
 function removeDuplicateChars(input) {
     // Chuyển chuỗi thành mảng các ký tự, sử dụng Set để lọc trùng lặp, rồi nối lại thành chuỗi
     return [...new Set(input)].join('');
 }
-
 // Test với các input
 console.log(removeDuplicateChars("banana")); // Output: "ban"
 console.log(removeDuplicateChars("hello world")); // Output: "helo wrd"
-
 // Áp dụng cho biến str
 let result = removeDuplicateChars(str);
 console.log(result); // Output: "helo wrd"
+//# sourceMappingURL=b7.js.map
